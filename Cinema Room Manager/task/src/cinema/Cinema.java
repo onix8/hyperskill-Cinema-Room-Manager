2e06c12c057cd81074e70e08f5b324b89d7e6ee6
ONIX <zachem-nado@mail.ru>
2021-11-02 00:17:54 +0300

    Stage 1/5: Arrangement (Solution)

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -2,7 +2,28 @@ package cinema;
 
 public class Cinema {
 
+    static char[][] room = {
+            {'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'},
+            {'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'},
+            {'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'},
+            {'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'},
+            {'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'},
+            {'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'},
+            {'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'}
+    };
+
     public static void main(String[] args) {
-        // Write your code here
+        printRoom();
+    }
+
+    private static void printRoom() {
+        System.out.println("Cinema:\n  1 2 3 4 5 6 7 8 ");
+        for (int i = 0; i < room.length; i++) {
+            System.out.print(i + 1);
+            for (int j = 0; j < room[i].length; j++) {
+                System.out.print(" " + room[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
