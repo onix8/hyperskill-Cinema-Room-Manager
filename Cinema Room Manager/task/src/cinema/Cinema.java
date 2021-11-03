0ac86b6db62aec5d605d77262166f8f1cb43ac57
ONIX <zachem-nado@mail.ru>
2021-11-03 16:49:30 +0300

    Stage 3/5: Tickets (Solution)

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -4,24 +4,67 @@ import java.util.Scanner;
 
 public class Cinema {
 
-    public static void main(String[] args) {
+    static Scanner sc = new Scanner(System.in);
+    static char[][] room;
 
+    public static void main(String[] args) {
+        setRoom();
+        printRoom();
+        buySeat();
+        printRoom();
+    }
+
+    private static void buySeat() {
+        int row;
+        int seat;
+        int price;
+
+        System.out.println("Enter a row number:");
+        row = sc.nextInt();
+        System.out.println("Enter a seat number in that row:");
+        seat = sc.nextInt();
+
+        room[row - 1][seat - 1] = 'B';
+
+        price = room.length * room[0].length <= 60 ? 10 : room.length / 2 < row ? 8 : 10;
+        System.out.printf("Ticket price: $%d\n", price);
+    }
+
+    private static void printRoom() {
+        int rows = room.length;
+        int seats = room[0].length;
+
+        System.out.print("Cinema:\n ");
+        for (int i = 1; i <= seats; i++) {
+            System.out.print(" " + i);
+        }
+        System.out.println();
+
+        for (int i = 0; i < rows; i++) {
+            System.out.print(i + 1);
+            for (int j = 0; j < seats; j++) {
+                System.out.print(" " + room[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    private static void setRoom() {
         int rows;
         int seats;
-        int income;
-        int totalSeats;
-
-        Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter the number of rows:");
         rows = sc.nextInt();
         System.out.println("Enter the number of seats in each row:");
         seats = sc.nextInt();
 
-        totalSeats = rows * seats;
-        income = totalSeats <= 60 ? totalSeats * 10 : ((rows / 2) * seats * 10) + ((rows - (rows / 2)) * seats * 8);
-
-        System.out.println("Total income:");
-        System.out.println("$" + income);
+        room = new char[rows][seats];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < seats; j++) {
+                room[i][j] = 'S';
+            }
+        }
+        System.out.println();
     }
 }
